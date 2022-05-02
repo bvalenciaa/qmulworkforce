30eb490be9974a63f90255331cf333fc317853c8
bvalenciaa <blanca.isabel.valencia@gmail.com>
2022-05-03 00:07:12 +0100

    edited views file

--- mysite/workforce/views.py
+++ mysite/workforce/views.py
@@ -6,12 +6,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
-from django.forms import inlineformset_factory
 from django.views import generic
 from datetime import date
-from django.utils.safestring import mark_safe
-import calendar
-from calendar import HTMLCalendar
 
 from .forms import *
 from .decorators import *
@@ -19,225 +15,13 @@ from .models import *
 from .filters import *
 
 
-# Create your views here.
-
-@login_required(login_url='login')
-def index(request):
-    team_id = request.user.member.team
-
-    members = Member.objects.all()
-
-    team_members = members.filter(team=team_id)
-
-    context = {'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/index.html', context)
-
-
-def sidebar(request):
-    team_id = request.user.member.team
-
-    members = Member.objects.all()
-
-    team_members = members.filter(team=team_id)
-
-    context = {'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/sidebar.html', context)
-
+# LANDING PAGE
 
 def landing(request):
     return render(request, 'workforce/landing.html')
 
 
-def home(request):
-    form = TaskForm()
-    member = request.user.member.id
-    tasks = Task.objects.all()
-    member_tasks = tasks.filter(user=member)
-    announcements = Announcement.objects.all()
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/home/')
-
-    context = {'member': member, 'tasks': tasks, 'member_tasks': member_tasks, 'form': form,
-               'announcements': announcements, 'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/home.html', context)
-
-
-def tasks(request):
-    tasks = Task.objects.all()
-
-    form = TaskForm()
-
-    myFilter = TaskFilter(request.GET, queryset=tasks)
-    tasks = myFilter.qs
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(request.path_info)
-    context = {'tasks': tasks, 'form': form, 'myFilter': myFilter, 'team_members': team_members, 'members': members,
-               'team_id': team_id}
-    return render(request, 'workforce/tasks.html', context)
-
-
-def hub(request):
-    deliverables = Deliverables.objects.all()
-
-    dev_count = deliverables.count()
-    dev_bar = (100 / dev_count)
-
-    teams = Team.objects.all()
-    form = DeliverablesForm()
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.method == 'POST':
-        form = DeliverablesForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/hub/')
-    context = {'deliverables': deliverables, 'dev_bar': dev_bar, 'teams': teams, 'form': form,
-               'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/hub.html', context)
-
-
-def workDetail(request, pk):
-    work = Work.objects.get(id=pk)
-    context = {'work': work}
-    return render(request, 'workforce/work_details.html', context)
-
-
-def addTeam(request):
-    form = addTeamForm()
-
-    if request.method == 'POST':
-        form = addTeamForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-
-    context = {'form': form}
-    return render(request, 'workforce/add_Team.html', context)
-
-
-def editDeliverable(request, pk):
-    deliverable = Deliverables.objects.get(id=pk)
-
-    form = DeliverablesForm(instance=deliverable)
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=deliverable)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-
-    context = {'form': form}
-    return render(request, 'workforce/edit_deliverable.html', context)
-
-
-def deleteDeliverable(request, pk):
-    deliverable = Deliverables.objects.get(id=pk)
-
-    if request.method == "POST":
-        deliverable.delete()
-        return redirect('/')
-
-    context = {'deliverable': deliverable}
-    return render(request, 'workforce/delete_deliverable.html', context)
-
-
-def work(request):
-    form = WorkForm()
-    works = Work.objects.all()
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.method == 'POST':
-        form = WorkForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(request.path_info)
-
-    context = {'form': form, 'works': works, 'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/work.html', context)
-
-
-def newWork(request):
-    form = WorkForm()
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.method == 'POST':
-        form = WorkForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(request.path_info)
-    context = {'form': form, 'team_members': team_members, 'members': members, 'team_id': team_id}
-    return render(request, 'workforce/new_work.html', context)
-
-
-def media(request):
-    uploads = Upload.objects.all()
-    form = UploadFileForm()
-
-    team_id = request.user.member.team
-    members = Member.objects.all()
-    team_members = members.filter(team=team_id)
-
-    if request.POST:
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/')
-
-    context = {'form': form, 'uploads': uploads, 'team_members': team_members, 'members': members, 'team_id': team_id}
-
-    return render(request, 'workforce/media.html', context)
-
-
-def editTask(request, pk):
-    task = Task.objects.get(id=pk)
-
-    form = TaskForm(instance=task)
-
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect('/tasks/')
-
-    context = {'form': form}
-    return render(request, 'workforce/edit_task.html', context)
-
-
-def deleteTask(request, pk):
-    task = Task.objects.get(id=pk)
-
-    if request.method == "POST":
-        task.delete()
-        return redirect('/tasks/')
-
-    context = {'task': task}
-    return render(request, 'workforce/delete_task.html', context)
-
+# Register and Login Views - can only be used if user is unauthenticated
 
 @unauthenticated_user
 def registerPage(request):
@@ -287,8 +71,30 @@ def logoutFunction(request):
     return redirect('login')
 
 
-def user(request):
-    return render(request, 'workforce/user.html')
+# INDEX
+# Index is the root location of the application, and loads the subsequent files
+
+@login_required(login_url='login')
+def index(request):
+    team_id = request.user.member.team
+
+    members = Member.objects.all()
+
+    team_members = members.filter(team=team_id)
+
+    context = {'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/index.html', context)
+
+
+def sidebar(request):
+    team_id = request.user.member.team
+
+    members = Member.objects.all()
+
+    team_members = members.filter(team=team_id)
+
+    context = {'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/sidebar.html', context)
 
 
 def userProfile(request):
@@ -302,6 +108,240 @@ def userProfile(request):
 
     context = {'form': form}
     return render(request, 'workforce/profile.html', context)
+
+
+# HOME
+# home contains all the individual information needed for the user
+
+@login_required(login_url='login')
+def home(request):
+    # Creates a new task form
+    form = TaskForm()
+    member = request.user.member.id
+    tasks = Task.objects.all()
+
+    # creates new filter for the tasks
+    member_tasks = tasks.filter(user=member)
+    announcements = Announcement.objects.all()
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+
+    context = {'member': member, 'tasks': tasks, 'member_tasks': member_tasks, 'form': form,
+               'announcements': announcements, 'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/home.html', context)
+
+
+# TASKS
+# tasks encapsulate all the task information related to all users
+
+def tasks(request):
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    # task filter is created to sort through tasks
+    myFilter = TaskFilter(request.GET, queryset=tasks)
+    tasks = myFilter.qs
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
+    context = {'tasks': tasks, 'form': form, 'myFilter': myFilter, 'team_members': team_members, 'members': members,
+               'team_id': team_id}
+    return render(request, 'workforce/tasks.html', context)
+
+
+def editTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/tasks/')
+
+    context = {'form': form}
+    return render(request, 'workforce/edit_task.html', context)
+
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    if request.method == "POST":
+        task.delete()
+        return redirect('/tasks/')
+
+    context = {'task': task}
+    return render(request, 'workforce/delete_task.html', context)
+
+# HUB
+# hub contains all information relating to the team and deliverables
+
+def hub(request):
+    deliverables = Deliverables.objects.all()
+
+    dev_count = deliverables.count()
+    dev_bar = (100 / dev_count)
+
+    teams = Team.objects.all()
+    form = DeliverablesForm()
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.method == 'POST':
+        form = DeliverablesForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/hub/')
+    context = {'deliverables': deliverables, 'dev_bar': dev_bar, 'teams': teams, 'form': form,
+               'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/hub.html', context)
+
+
+def addTeam(request):
+    form = addTeamForm()
+
+    if request.method == 'POST':
+        form = addTeamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'workforce/add_Team.html', context)
+
+
+def editDeliverable(request, pk):
+    deliverable = Deliverables.objects.get(id=pk)
+
+    form = DeliverablesForm(instance=deliverable)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=deliverable)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'workforce/edit_deliverable.html', context)
+
+
+def deleteDeliverable(request, pk):
+    deliverable = Deliverables.objects.get(id=pk)
+
+    if request.method == "POST":
+        deliverable.delete()
+        return redirect('/')
+
+    context = {'deliverable': deliverable}
+    return render(request, 'workforce/delete_deliverable.html', context)
+
+# WORK
+# work has all the work files relating to the project
+
+def work(request):
+    form = WorkForm()
+    works = Work.objects.all()
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.method == 'POST':
+        form = WorkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
+
+    context = {'form': form, 'works': works, 'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/work.html', context)
+
+
+def newWork(request):
+    form = WorkForm()
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.method == 'POST':
+        form = WorkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(request.path_info)
+    context = {'form': form, 'team_members': team_members, 'members': members, 'team_id': team_id}
+    return render(request, 'workforce/new_work.html', context)
+
+
+def workDetail(request, pk):
+    work = Work.objects.get(id=pk)
+    context = {'work': work}
+    return render(request, 'workforce/work_details.html', context)
+
+
+def editWork(request, pk):
+    work = Work.objects.get(id=pk)
+
+    form = TaskForm(instance=work)
+
+    if request.method == 'POST':
+        form = WorkForm(request.POST, instance=work)
+        if form.is_valid():
+            form.save()
+            return redirect('/work/')
+
+    context = {'form': form}
+    return render(request, 'workforce/edit_work.html', context)
+
+
+def deleteWork(request, pk):
+    work = Work.objects.get(id=pk)
+
+    if request.method == "POST":
+        work.delete()
+        return redirect('/work/')
+
+    context = {'work': work}
+    return render(request, 'workforce/delete_work.html', context)
+
+# MEDIA
+# media contains all relevant media files
+
+def media(request):
+    uploads = Upload.objects.all()
+    form = UploadFileForm()
+
+    team_id = request.user.member.team
+    members = Member.objects.all()
+    team_members = members.filter(team=team_id)
+
+    if request.POST:
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form, 'uploads': uploads, 'team_members': team_members, 'members': members, 'team_id': team_id}
+
+    return render(request, 'workforce/media.html', context)
 
 
 def editFile(request, pk):
@@ -330,27 +370,5 @@ def deleteFile(request, pk):
     return render(request, 'workforce/delete_file.html', context)
 
 
-def editWork(request, pk):
-    work = Work.objects.get(id=pk)
-
-    form = TaskForm(instance=work)
-
-    if request.method == 'POST':
-        form = WorkForm(request.POST, instance=work)
-        if form.is_valid():
-            form.save()
-            return redirect('/work/')
-
-    context = {'form': form}
-    return render(request, 'workforce/edit_work.html', context)
-
-
-def deleteWork(request, pk):
-    work = Work.objects.get(id=pk)
-
-    if request.method == "POST":
-        work.delete()
-        return redirect('/work/')
-
-    context = {'work': work}
-    return render(request, 'workforce/delete_work.html', context)
+def user(request):
+    return render(request, 'workforce/user.html')
