137a5b23af44957ae4b639eb857db63287d08aec
bvalenciaa <blanca.isabel.valencia@gmail.com>
2022-05-02 20:49:46 +0100

    heroku requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+@lw1)g71z%el1xqe5_dqqj4pd7zm+&0ahq8@lx&l)0@+p*9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://qmulworkforce.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
